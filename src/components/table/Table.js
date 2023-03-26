c69bf5f44e8838c0a23a8013566f913f5d86a131
Yuri <playeer2905@gmail.com>
2023-03-26 10:21:32 +0300

    resize finished

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,7 @@
 import { $ } from "../../core/dom";
 import { ExcelComponent } from "../../core/ExcelComponent";
+import { shouldResize } from "./table.function";
+import { resizeHandler } from "./table.resize";
 import { createTable } from "./table.template";
 
 export class Table extends ExcelComponent {
@@ -16,34 +18,8 @@ export class Table extends ExcelComponent {
     }
 
     onMousedown(event) { 
-        if (event.target.dataset.resize) {
-            const $resizer = $(event.target) // получаем instance класса dom
-            // const $parent = $resizer.parentNode // bad так как в таком случае мы зависим от верстки и в случае ее изменения придется менять логику
-            // const $parent = $resizer.closest('.column') // better but bad
-            const $parent = $resizer.closest('[data-type="resizable"]')
-            const coords = $parent.getCoords()
-            const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-            const type = $resizer.data.resize
-            if (type === 'col') {
-                document.onmousemove = e => {
-                    const delta = e.pageX - coords.right
-                    const value = coords.width + delta
-                    // $parent.$el.style.width = value + 'px'
-                    $parent.css({ width: value + 'px'})
-                    // console.dir($parent.$el)
-                    cells.forEach(el => el.style.width = value + 'px')
-                }
-            } else {
-                document.onmousemove = e => {
-                    const delta = e.pageY - coords.bottom
-                    const value = coords.height + delta
-                    // $parent.$el.style.height = value + 'px'
-                    $parent.css({height: value + 'px'})
-                }
-            }
-            document.onmouseup = () => {
-                document.onmousemove = null
-            }
+        if (shouldResize(event)) {
+            resizeHandler(event, this.$root)
         }
     }
 }
---
+++ src/components/table/table.resize.js
@@ -0,0 +1,39 @@
+import { $ } from "../../core/dom"
+
+export function resizeHandler(event, root) {
+    if (event.target.dataset.resize) {
+        const $resizer = $(event.target) // получаем instance класса dom
+        // const $parent = $resizer.parentNode // bad так как в таком случае мы зависим от верстки и в случае ее изменения придется менять логику
+        // const $parent = $resizer.closest('.column') // better but bad
+        $resizer.css({opacity: 1})
+        const $parent = $resizer.closest('[data-type="resizable"]')
+        const coords = $parent.getCoords()
+        const cells = root.findAll(`[data-col="${$parent.data.col}"]`)
+        const type = $resizer.data.resize
+        let value = 0
+        if (type === 'col') {
+            document.onmousemove = e => {
+                const delta = e.pageX - coords.right
+                value = coords.width + delta
+                $resizer.css({ left: value - 4 + 'px'})
+            }
+        } else {
+            document.onmousemove = e => {
+                const delta = e.pageY - coords.bottom
+                value = coords.height + delta
+                $resizer.css({ top: value - 4 + 'px'})
+            }
+        }
+        document.onmouseup = () => {
+            if (type === 'col') {
+                $parent.css({ width: value + 'px'})
+                cells.forEach(el => el.style.width = value + 'px')
+            } else {
+                $parent.css({height: value + 'px'})
+            }
+            $resizer.css({opacity: 0})
+            document.onmousemove = null
+            document.onmouseup = null
+        }
+    }
+}
